0e47b1a2a22a3288984e7f75c8af7a40aeb3edb0
Ethan Morgan <etcmo@umich.edu>
2021-12-17 17:33:04 -0500

    working Card reader

--- readParser.py
+++ readParser.py
@@ -1,26 +1,27 @@
 import re
 #%B6008475547260813^MORGAN/E^2412120ETCMO?;6008475547260813=2412120=302135592?
-#
+#Card Num          ^ LastName/First inital^ num+uniquname?;
 #%B6008473657049217^AGRAWAL/R^2305120RAGRAW?;6008473657049217=2305120=439169501?
+#%B6008473507106514^ZOU/H^2608120TINAZOU?;6008473507106514=2608120=349611241?
+
 
 def parseCardInfo(cardReaderInput):
 
     #validation regex: 
-    if not re.match('%B([0-9]{,19})\^[[:alpha:]]{2,26}\/[[:alpha:]]\^[[:alnum:]]*?;*', cardReaderInput):
+    if not re.match("%B([0-9]){16}\^[A-Z]{2,26}\/[A-Z]\^([0-9]){7}([A-Z])*\?\;*", cardReaderInput):
         print("Reenter Card Info")
         return None
     # re.split('%B([0-9]{,19})\^[[:alpha:]]{2,26}\/[[:alpha:]]\^[[:alnum:]]*?;*', x))
-    cardInfo = re.split('(\W)', cardReaderInput)
-    cardNum = cardInfo[2][1:]
-    lastName = cardInfo[4]
-    firstInital = cardInfo[6]
-    uniquename = cardInfo[8][7:]
-    return uniquename
+    cardInfo = re.compile('([A-Z]+)').split(cardReaderInput)
+    lastName = cardInfo[3]
+    firstInitial = cardInfo[5]
+    uniquename = cardInfo[7]
+    return lastName, firstInitial, uniquename
     
 
 
 
-# parseCardInfo('%B6008475547260813^MORGAN/E^2412120ETCMO?;6008475547260813=2412120=302135592?')
+# print(parseCardInfo('%B6008475547260813^MORGAN/E^2412120ETCMO?;6008475547260813=2412120=302135592?'))
 x = input()
 while(x != quit):
     print(parseCardInfo(x))
